5c91399fa845ee59df9ef93e6677eb1982da48de
Jack625a <ktfut15@gmail.com>
2025-03-28 14:47:23 -0400

    Barra de Navegacion

--- layouts/src/main.py
+++ layouts/src/main.py
@@ -2,8 +2,53 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    #page.title("Navegacion")
+    page.navigation_bar=ft.CupertinoNavigationBar(
+        #bgcolor=ft.Colors.GREEN_700,
+        #inactive_color=ft.Colors.YELLOW_700,
+        #active_color=ft.Colors.RED_500,
+        destinations=[
+            ft.NavigationBarDestination(
+                label="Inicio",
+                icon=ft.Icons.HOME,
+            ),
+            ft.NavigationBarDestination(
+                label="Productos",
+                icon=ft.Icons.STORE
+            ),
+            ft.NavigationBarDestination(
+                label="Ajustes",
+                icon=ft.Icons.ACCESS_ALARM_OUTLINED
+            ),
+            ft.NavigationBarDestination(
+                label="Perfil",
+                icon=ft.Icons.ACCOUNT_TREE
+            )
+        ]
+    )
+
+    drawer=ft.NavigationDrawer(
+        controls=[
+            ft.Container(height=15),
+            ft.NavigationDrawerDestination(
+                label="Inicio",
+                icon=ft.Icons.HOME_MAX_SHARP
+            ),
+            ft.Divider(thickness=2),
+            ft.NavigationDrawerDestination(
+                label="Servicios",
+                icon=ft.Icons.ACCOUNT_BALANCE_ROUNDED
+            ),
+            ft.Divider(thickness=2),
+            ft.NavigationDrawerDestination(
+                label="Opcion",
+                icon=ft.Icons.MENU
+            ),
+        ],
+    )
    
     page.add(
+        ft.CupertinoFilledButton(text="mostrar", on_click=lambda e:page.open(drawer)),
         ft.Row([
             ft.Container(
                 content=ft.Text("Prueba"),
