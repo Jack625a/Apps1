e2267c0628d9cb9a4c685856fb3efdcb8083070c
Jack625a <ktfut15@gmail.com>
2025-05-02 14:47:59 -0400

    base de datos INSERCION

--- baseDatos/src/main.py
+++ baseDatos/src/main.py
@@ -23,7 +23,21 @@ def obtenerEstudinates():
         print("Error al obtner los datos")
         return []
 
+#Agregar datos (insercion)
+def agregarEstudiantes(nombre,carrera,edad,imagen):
+    data={
+        "fields":{
+            "nombre":nombre,
+            "carrera":carrera,
+            "edad":edad,
+            "imagen":imagen
+        }
+    }
+    respuesta=requests.post(urlAirtable, headers=HEADERS, json=data)
+    return respuesta.status_code==200 or respuesta.status_code==201
+
 def main(page: ft.Page):
+    resultado=ft.Text()
 
     def mostrarEstudiantes(evento):
         registros=obtenerEstudinates()
@@ -31,7 +45,8 @@ def main(page: ft.Page):
         for i in registros:
             datos=i['fields']
             lista.controls.append(
-            ft.Text(f"{datos.get('nombre','')} - {datos.get('carrera','')} -{datos.get('edad','')} ")   
+            ft.Text(f"{datos.get('nombre','')} - {datos.get('carrera','')} -{datos.get('edad','')} - url Imagen {datos.get('imagen')}"),
+            
             )
         page.update()
 
@@ -42,10 +57,32 @@ def main(page: ft.Page):
         )
     lista=ft.Column()
 
+    #campos de entrada 
+    nombre=ft.TextField(label="Ingrese su nombre: ")
+    carrera=ft.TextField(label="Ingrese su carrera: ")
+    edad=ft.TextField(label="Ingrese su edad: ")
+    imagen=ft.TextField(label="Ingrese su imagen: ")
 
-  
+    #funcion para guardar los datos en Airtable
+    def guardarDatos(e):
+        exitoso=agregarEstudiantes(nombre.value,carrera.value,edad.value,imagen.value)
+        if exitoso:
+            resultado.value="Estudiante agregado correctamente"
+            nombre.value=carrera.value=edad.value=imagen.value=""
+            obtenerEstudinates()
+        else:
+            resultado.value="Error al agregar los datos..."
+        page.update()
+
+
     page.add(
-        botonMostrar,lista
+        botonMostrar,lista,
+        #formulario para enviar datos al servidor
+        ft.Divider(),
+        ft.Text("Agregado de Estudiantes", size=25),
+        nombre,carrera,edad,imagen,
+        ft.CupertinoFilledButton("Agregar Estudiante", on_click=guardarDatos),
+        resultado
         
     )
 
